eca348591d0f576fdffdc79589a86890dd3f75c9
Resnog <sergio.m.gonzalez.a.94@gmail.com>
2023-09-28 23:59:04 +0300

    Add error codes for YAPP

---
+++ inc/yapp.hpp
@@ -0,0 +1,20 @@
+
+#ifndef YAPP_H
+#define YAPP_H
+
+#include <array>
+#include <SFML/Graphics.hpp>
+
+namespace YAPP{
+
+    #define YAPP_DEF_RESOLUTION_X 800
+    #define YAPP_DEF_RESOLUTION_Y 600
+
+    enum YAPP_ERR{
+        YAPP_OK,
+        YAPP_MAP_LOADED,
+        YAPP_MAP_LOAD_ERR,
+    };
+
+}
+#endif // YAPP_DISPLAY_H
--- src/yapp.cpp
+++ src/yapp.cpp
@@ -2,26 +2,26 @@
 #include <iostream>
 #include <vector>
 #include "display.hpp"
+#include "yapp.hpp"
 
-#define YAPP_DEF_RESOLUTION_X 800
-#define YAPP_DEF_RESOLUTION_Y 600
 
 int main(int argc, char *argv[])
 {
+
     // Main YAPP window
     sf::RenderWindow window(sf::VideoMode(YAPP_DEF_RESOLUTION_X, 
                                           YAPP_DEF_RESOLUTION_Y),
                                           "Yet Another Path Planner");
 
-    defaultMap map(&window);
-    
+    YAPP::defaultMap map(&window);
+
     // TODO Create the algorithm solver, an easy start would be A-Star
     // since it is done in another repo
 
     // Map display loop
     while (window.isOpen())
     {
-        window.clear();
+        
         sf::Event event;
         // Event loop
         while (window.pollEvent(event))
@@ -30,8 +30,10 @@ int main(int argc, char *argv[])
 
                 window.close();
             
-            // TODO: Make an event to handle the map update
         }
+
+        window.clear();
+
         // NOTE: This map display is for testing the map, not the final state
         map.drawMap();
 
