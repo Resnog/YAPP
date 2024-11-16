2712cbd5a83e983562ebbfc4272090af228e5e12
Resnog <sergio.m.gonzalez.a.94@gmail.com>
2024-11-17 00:09:40 +0200

    Add map for YAPP
    I need to start by reading some map from a file, I'll take a CSV file
    for now and use this as a grid. This commit sets the boilerplate code to
    read a map from a file without actual implementation.

---
+++ src/map.rs
@@ -0,0 +1,15 @@
+pub mod map {
+    use std::path;
+
+    struct Map {
+        resolution: u64,
+        width: u64,
+        height: u64,
+    }
+
+    impl Map {
+        pub fn readMap(file: Path) {
+            todo!();
+        }
+    }
+}
